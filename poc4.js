a38623b35a222ff7333b7af015a9da29a4861d9f
huss0249 <ahmedsabry@outlook.com>
2023-08-21 00:04:39 -0400

    done 1

--- poc4.js
+++ poc4.js
@@ -42,9 +42,13 @@ const reuse = {
 
 // ======================================================
 class FireContainer {
-    constructor(_w, _h, _i, _obj) {
+    // constructor(_w, _h, _i, _obj) {
+    constructor(_i, _obj) {
         this.app = _obj
         this._i = _i
+        this.abc = this
+        // this.aa = this.aa.bind(this) 
+        this.$fire = ''
 
         this.app.classList.add(
           "row",
@@ -71,6 +75,7 @@ class FireContainer {
             $flame.style.setProperty("--rand", Math.random());
             $fire.appendChild($flame)
         }
+        this.$fire = $fire
         return $fire
     }
 
@@ -92,27 +97,74 @@ class FireContainer {
     }
 
     makeFire(_i) {
-        this.app.appendChild(FireContainer.createContainer(this._i));
+      this.app.appendChild(FireContainer.createContainer(this._i));
+      // log(this)
     }
 
-    aa() {
+    fireUp() {
+      // let $fire = 
         // $fire.classList.remove("fire-fade");
         // $fire.classList.add("fire-up");
+        
+        FireContainer.$fire.classList.remove("fire-fade");
+        FireContainer.$fire.classList.add("fire-up");
         // log(e.target);
-        log('fire up');
+      log(FireContainer.$fire);
     }
     
-    
-    // fireDown(e) {
+    fireDown() {
+      FireContainer.$fire.classList.remove("fire-fade");
+      FireContainer.$fire.classList.add("fire-down");
+      // log(e.target);
+      log(FireContainer.$fire);
+
     //     $fire.classList.remove("fire-fade");
     //     $fire.classList.add("fire-down");
     //     log(e.target);
-    // }
+    }
+    
+    reset() {
+      FireContainer.$fire.classList.remove("fire-fade", "fire-up", "fire-down");
+      
+        
+      log('reset');
+    }
+    
+    stop() {
+      // FireContainer.$fire.classList.remove("fire-fade");
+      FireContainer.$fire.classList.add("fire-fade");
+      log('stop');
+    }
+
 }
 
-const s1 = new FireContainer(5, 6, 70, $App1).makeFire()
-const s2 = new FireContainer(5, 6, 50, $App2).makeFire()
-const s3 = new FireContainer(5, 6, 40, $App3).makeFire()
-const s4 = new FireContainer(5, 6, 30, $App4).makeFire()
+const s1 = new FireContainer(70, $App1)
+// const s2 = new FireContainer(50, $App2)
+// const s3 = new FireContainer(40, $App3)
+// const s4 = new FireContainer(30, $App4)
 
-s1.aa()
+s1.makeFire()
+// s2.makeFire()
+// s3.makeFire()
+// s4.makeFire()
+
+// s1.fireUp()
+// const s1 = new FireContainer(5, 6, 70, $App1).makeFire()
+// const s2 = new FireContainer(5, 6, 50, $App2).makeFire()
+// const s3 = new FireContainer(5, 6, 40, $App3).makeFire()
+// const s4 = new FireContainer(5, 6, 30, $App4).makeFire()
+
+
+// const $up = document.getElementById("up")
+// $up.addEventListener('click', function() {
+//   s1.fireUp()
+//   s2.fireDown()
+//   s3.fireDown()
+//   s4.fireDown()
+// })
+
+// $up.addEventListener('click', s1.fireUp)
+document.getElementById("up").addEventListener('click', s1.fireUp)
+document.getElementById("down").addEventListener('click', s1.fireDown)
+document.getElementById("stop").addEventListener('click', s1.stop)
+document.getElementById("reset").addEventListener('click', s1.reset)
